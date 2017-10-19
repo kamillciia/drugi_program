935335c587066d27b3fed2fe37ae22fb444d7454
kamillciia <kamillciia@o2.pl>
2017-10-19 17:47:45 +0200

    drugi_program

---
+++ program2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+int main()
+{
+	putchar('v'); /*wypisz znak*/
+
+	putchar('\n');/*znak konca lini*/
+
+	puts("wypisz string / ciag znakow");
+
+	puts("\nAla ma kota, \na kot ma Ale");
+
+	return 0;
+
+}
+
